22919fcee510db4f091a6db2a24935830327dd98
Exhaler <fadibar@gmail.com>
2025-08-15 18:39:20 -0400

    Adjust ingrdients

--- prototypes/recipe/cursix-recipe.lua
+++ prototypes/recipe/cursix-recipe.lua
@@ -7,9 +7,9 @@ data:extend(
       energy_required = 30,
       ingredients =
       {
-        { type = "item", name = "copper-plate", amount = 10 },
-        { type = "item", name = "copper-cable", amount = 50 },
-        { type = "item", name = "wood",         amount = 5 },
+        { type = "item", name = "iron-plate",   amount = 100 },
+        { type = "item", name = "copper-plate", amount = 100 },
+        { type = "item", name = "wood",         amount = 25 },
       },
       results = {
         { type = "item", name = "cursix-armor", amount = 1 },
@@ -22,9 +22,9 @@ data:extend(
       energy_required = 10,
       ingredients =
       {
-        { type = "item", name = "copper-plate", amount = 10 },
-        { type = "item", name = "copper-cable", amount = 50 },
-        { type = "item", name = "wood",         amount = 5 },
+        { type = "item", name = "iron-plate",         amount = 10 },
+        { type = "item", name = "electronic-circuit", amount = 5 },
+        { type = "item", name = "cursix-mushroom",    amount = 1 },
       },
       results = {
         { type = "item", name = "cursix-beacon", amount = 1 },
