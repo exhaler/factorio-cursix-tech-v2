ddeb77a841f69634654a59e5937d6d56bfd0cec0
Exhaler <fadibar@gmail.com>
2025-08-16 11:53:59 -0400

    lint

--- migrations/cursix-tech_0.2.1.lua
+++ migrations/cursix-tech_0.2.1.lua
@@ -1,13 +1,13 @@
 -- Enable new recipes Cursix Technology unlocks
-for i, force in pairs(game.forces) do 
-  if force.technologies["cursix-technology"].researched then 
-    force.recipes["barrier-jacket-shield-equipment"].enabled = true
-	force.recipes["energy-storage-crystal-equipment"].enabled = true
-	force.recipes["sonic-scrambled-eggs-equipment"].enabled = true
-	force.recipes["sonic-speed-shoes-equipment"].enabled = true
-	force.recipes["cursix-mushroom"].enabled = true
-	force.recipes["cursix-1up-mushroom"].enabled = true
-	force.recipes["cursix-poison-mushroom"].enabled = true
-	force.recipes["cursix-armor"].enabled = true
-  end
+for i, force in pairs(game.forces) do
+	if force.technologies["cursix-technology"].researched then
+		force.recipes["barrier-jacket-shield-equipment"].enabled = true
+		force.recipes["energy-storage-crystal-equipment"].enabled = true
+		force.recipes["sonic-scrambled-eggs-equipment"].enabled = true
+		force.recipes["sonic-speed-shoes-equipment"].enabled = true
+		force.recipes["cursix-mushroom"].enabled = true
+		force.recipes["cursix-1up-mushroom"].enabled = true
+		force.recipes["cursix-poison-mushroom"].enabled = true
+		force.recipes["cursix-armor"].enabled = true
+	end
 end
